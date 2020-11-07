30e442bbe356be49d27114c032921d2ea86ae67f
sikiryl <saritrat.206@gmail.com>
2020-11-07 21:01:24 +0700

    init dotnet core

---
+++ Controllers/KubeOpsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//     WebHost.CreateDefaultBuilder(args)
+//         .ConfigureAppConfiguration(c =>
+//         {
+//            c.AddJsonFile("config/appsettings.json", optional: true, reloadOnChange: true);
+//         })
+//         .UseStartup<Startup>();
+
+namespace dotnet_core_web_api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class KubeOpsController : ControllerBase
+    {
+        private readonly ILogger<KubeOpsController> _logger;
+
+        public KubeOpsController(ILogger<KubeOpsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello KubeOps Skills Dotnet Core !!";
+        }
+    }
+}
