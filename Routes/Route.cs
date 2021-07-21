0250f0e305ca884d56eb4ca3f97d70863539e1f3
dmakeroam <cpfapp@gmail.com>
2021-07-21 20:26:03 +0700

    refactored code

---
+++ Routes/Route.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using DotNetCoreWebAPI.Controllers;
+namespace DotNetCoreWebAPI.Routes
+{
+    public class Route
+    {
+        public static void Setup(WebApplication app)
+        {
+            app.MapGet("/", KubeOpsController.Get);
+        }
+    }
+
+}
